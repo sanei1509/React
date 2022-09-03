0f432dc6bd2c5333d39f49f5cc9e6d1b90996e39
santiago <santiagoneira52@gmail.com>
2022-09-03 00:38:12 -0300

    testing things of react router

--- proyectos/vite-project/src/App.jsx
+++ proyectos/vite-project/src/App.jsx
@@ -1,19 +1,34 @@
 import { useState } from "react";
 import "./index.css";
+// import { Route, Routes } from "react-router-dom";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // importo los componentes que voy a utilizar
-import Header from "./componentes/Header";
-import Formulario from "./componentes/Formulario";
-import ListadoPacientes from "./componentes/ListadoPacientes";
+// import Header from "./componentes/Header";
+// import Formulario from "./componentes/Formulario";
+// import ListadoPacientes from "./componentes/ListadoPacientes";
+
+//importaciones del ruteo
+import Home from "./vistas/Home";
+import Login from "./vistas/Login";
+import Register from "./vistas/Register";
 
 function App() {
   // podemos trabajar lógica por aqui
-
   return (
     <div className="App" style={{ width: "100%", alignContent: "center" }}>
-      <Header />
+      <Routes>
+        {/* HOME */}
+        <Route path="/" element={<Home />} />
+        {/* LOGIN */}
+        <Route path="/login" element={<Login />} />
+        {/* REGISTER */}
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      {/* <Header />
       <Formulario />
-      <ListadoPacientes />
+      <ListadoPacientes /> */}
     </div>
   );
 }

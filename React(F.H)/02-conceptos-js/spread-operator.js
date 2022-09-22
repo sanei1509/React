c0bdebec083d277cf751caf41f4b6519e715b5de
santiago <santiagoneira52@gmail.com>
2022-09-22 09:59:47 -0300

    update course of react

--- React(F.H)/02-conceptos-js/spread-operator.js
+++ React(F.H)/02-conceptos-js/spread-operator.js
@@ -1,6 +1,8 @@
 // UTILIDAD:
 // REST -> extender los elementos de más pasados
-// SPREAD -> hacer una copia exacta de un grupo de elementos
+// SPREAD:
+// -> hacer una copia exacta de un grupo de elementos
+// -> expandir uno a uno una lista de elementos
 function sumar(a, b, ...others) {
   //   queremos sumar los 2 números principales y todo lo que se me pueda pasar
   let total = 0;
@@ -16,3 +18,12 @@ function sumar(a, b, ...others) {
 const nums = [2, 2, 2, 2, 2, 5, 10];
 
 console.log(sumar(...nums));
+
+// Probando el spread operator
+const lista = [1, 2, 3, 4];
+
+// queremos extenderlo a 5
+const copiaExacta = [...lista, 5, 6];
+console.log(copiaExacta);
+
+// function construirFrase()

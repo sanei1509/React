9f544240b050d305df02b8900852f7c7570b9f86
santiago <santiagoneira52@gmail.com>
2022-09-22 09:17:46 -0300

    add some test to vite project

--- proyectos/vite-project/tests/demo.test.js
+++ proyectos/vite-project/tests/demo.test.js
@@ -4,3 +4,18 @@ test("Esto no debe fallar", () => {
   // Queremos evaluar algo
   if (2 < 1) throw new Error("no puede ser");
 });
+
+test("Los mensajes deberíans ser iguales", () => {
+  // 1.incialización
+  const mensaje_uno = "Hola";
+
+  // 2. Cambio / estimulo
+  let mensaje_dos = "Hola ";
+
+  // solución :
+  // pasarle un .trim al mensaje_dos
+  mensaje_dos = mensaje_dos.trim();
+
+  // 3. Observar el comportamiento que esperamos
+  expect(mensaje_uno).toBe(mensaje_dos);
+});

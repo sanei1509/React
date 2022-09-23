f890b530d6553accd9a987c593bb5fe2e499a9e3
santiago <santiagoneira52@gmail.com>
2022-09-23 10:42:42 -0300

    add test to 07 file of js

--- proyectos/vite-project/tests/base-pruebas/07-deses-arr.test.js
+++ proyectos/vite-project/tests/base-pruebas/07-deses-arr.test.js
@@ -1,3 +1,14 @@
-import { random_array, usState } from "../../src/base-pruebas";
+import { random_array, usState } from "../../src/base-pruebas/07-deses-arr";
 
-describe("Testeo funciones de 07-deses.arr.js", () => {});
+describe("Testeo funciones de 07-deses.arr.js", () => {
+  test("debe devolver un array con solo strings", () => {
+    // Inicializamos con mi función
+    const [string, numero] = random_array();
+
+    // Controlamos el tipo dentro del string
+    expect(typeof string).toBe("string");
+    expect(typeof numero).toBe("number");
+
+    // le especifico que voy a esperar cualquier string ninguno fijo
+  });
+});

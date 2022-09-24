7f90ca2e6afcea46f19910640fca8d71b936010a
santiago <santiagoneira52@gmail.com>
2022-09-24 20:58:10 -0300

    progress in app of gifs

--- proyectos/gifs-app/src/AppContainer.jsx
+++ proyectos/gifs-app/src/AppContainer.jsx
@@ -1,5 +1,21 @@
 // ESTE COMPONENTE ES EL CONTENEDOR DE NUESTRA APP
+import { useState } from "react";
 
 export const AppContainer = () => {
-  return <h1>App en construcción</h1>;
+  // incializamos las categorías con un hook para poder modificar
+  const [temas, setTemas] = useState(["hola", "hola2", "hola3"]);
+  return (
+    <div>
+      <h1>Busca tus gifs favoritos aqui !</h1>
+
+      {/* INPUT DE BUSQUEDA */}
+
+      {/* LISTAMOS TODOS LOS GIF CORRESPONDIENTES A LA BUSQUEDA */}
+      <ol>
+        {temas.map((tema) => {
+          return <li>Hola</li>;
+        })}
+      </ol>
+    </div>
+  );
 };

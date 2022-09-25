dd6e84f2cc1f2b44618d17664bdaf94ef350bbe8
santiago <santiagoneira52@gmail.com>
2022-09-24 21:00:25 -0300

    update the files of js to adapt for the tests

--- proyectos/vite-project/src/base-pruebas/08-imp-exp.js
+++ proyectos/vite-project/src/base-pruebas/08-imp-exp.js
@@ -1,20 +1,34 @@
+// DISTINTAS IMPORTACIONES
 // import { heroes } from './data/heroes';
 // import { heroes } from './data/heroes';
 // import heroes, { owners } from '../data/heroes';
-import heroes from '../data/heroes';
+
+// array de objetos
+const heroes = [
+  //heroe 1
+  {
+    id: 1,
+    name: "Bruce Wayne",
+    owner: "DC",
+  },
+  //   heroe 2
+  {
+    id: 2,
+    name: "Clark Kent",
+    owner: "MARVEL",
+  },
+];
 
 // console.log( owners );
 
-
-
-
-export const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
-
+// BUSCADOR DE HEROE POR ID EN UN OBJETO
+export const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
+// console.log(getHeroeById(2));
 // console.log( getHeroeById(2) );
 
 // find?, filter
-export const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
+// BUSCADOR DE HEROE POR ID EN UN OBJETO
+export const getHeroesByOwner = (owner) =>
+  heroes.filter((heroe) => heroe.owner === owner.toUpperCase());
 
-// console.log( getHeroesByOwner('Marvel') );
-
-
+// console.log(getHeroesByOwner("marvel"));
--- proyectos/vite-project/src/base-pruebas/11-async-await.js
+++ proyectos/vite-project/src/base-pruebas/11-async-await.js
@@ -1,32 +1,23 @@
-
-
 // const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
 // getImagenPromesa().then( console.log );
 
-const getImagen = async() => {
+const getImagen = async () => {
+  try {
+    const apiKey = "C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC";
+    const resp = await fetch(
+      `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+    );
+    const { data } = await resp.json();
 
-    try {
+    const { url } = data.images.original;
 
-        const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
-        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-        const { data } = await resp.json(); 
+    const img = document.createElement("img");
+    img.src = url;
+    document.body.append(img);
+  } catch (error) {
+    // manejo del error
+    console.error(error);
+  }
+};
 
-        const { url } = data.images.original;
-
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
-
-    } catch (error) {
-        // manejo del error
-        console.error(error)
-    }
-    
-    
-    
-}
-
- getImagen();
-
-
-
+getImagen();

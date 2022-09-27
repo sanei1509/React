46b3b0e81298d056fc1dff33a13ed4c42dc2196d
santiago <santiagoneira52@gmail.com>
2022-09-27 12:17:30 -0300

    testing the counterApp component

--- proyectos/vite-project/tests/CounterApp.test.js
+++ proyectos/vite-project/tests/CounterApp.test.js
@@ -4,7 +4,7 @@
 // 1 - debe hacer match con el snapshot
 // 2 - debe de mostrar el valor inicial de 0
 // IMPORTACION DE FILE
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import CounterApp from "../src/CounterApp.jsx";
 
 describe("Pruebas sobre el contador - CounterApp", () => {
@@ -23,5 +23,29 @@ describe("Pruebas sobre el contador - CounterApp", () => {
 
     // tambien podemos comprobar el elemento
     expect(screen.getByRole("heading", { level: 2 }.innerHTML).toContain("0"));
+  });
+
+  // PRUEBAS SOBRE EVENTOS (RESULTADO DE ELLOS)
+  test("debería aumentar en 1 con el botón +1", () => {
+    render(<CounterApp valor={10} />);
+    fireEvent.click(screen.getByText("+1"));
+    expect(screen.getByText("11")).toBeTruthy();
+  });
+
+  test("debería decrementar en 1 con el botón -1", () => {});
+  render(<CounterApp value={10} />);
+  fireEvent.click(screen.getByText("-1")).toBeTruthy();
+  expect(screen.get);
+
+  test("Debe estar funcionando el boton de reset, debería volver a ser 10", () => {
+    render(<CounterApp valor={10} />);
+    fireEvent.click(screen.getByText("+1"));
+    fireEvent.click(screen.getByText("+1"));
+    fireEvent.click(screen.getByText("+1"));
+    fireEvent.click(screen.getByText("RESET"));
+
+    // enlazar un boton especifico
+    // fireEvents.click(screen.getByRole("button", { name: 'btn-reset'}))
+    expect(screenl.getByText(10)).toBeTruthy();
   });
 });

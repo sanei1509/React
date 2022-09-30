76d1fcdf0793124d83563874d971e908aa25f711
santiago <santiagoneira52@gmail.com>
2022-09-30 11:06:16 -0300

    include tailwind into the project

--- proyectos/gifs-app/src/AppContainer.jsx
+++ proyectos/gifs-app/src/AppContainer.jsx
@@ -3,28 +3,38 @@ import { useState } from "react";
 
 // componentes personalizados
 import { AddTopic } from "./components/AddTopic";
+import "./style.css";
 
 export const AppContainer = () => {
   // incializamos las categorías con un hook para poder modificar
-  const [temas, setTemas] = useState(["hola", "hola2", "hola3"]);
+  const [temas, setTemas] = useState(["Familia", "Amigos", "Descontrol!"]);
 
   const handlerTopic = () => {
     console.log("Hoola");
-    setTemas([...temas, "hola4"]); // añadimos un nuevo tema
+    // setTemas([...temas, "Te Quiero!"]);
   };
 
   return (
-    <div>
-      <h1>Busca tus gifs favoritos aqui !</h1>
-      <AddTopic />
+    <div className="max-w-4xl m-auto content-center text-center mt-4">
+      <h1 className="text-3xl font-mono">Busca tus gifs favoritos aqui !</h1>
+      <AddTopic setTemas={setTemas} añadidos={temas} />
       {/* INPUT DE BUSQUEDA */}
 
       {/* LISTAMOS TODOS LOS GIF CORRESPONDIENTES A LA BUSQUEDA */}
 
-      <button onClick={handlerTopic}>Añadir</button>
-      <ol>
+      <button
+        className="inline-flex items-center justify-center rounded-md bg-slate-800 mt-2 pr-10 pl-10 pb-2 pt-2 text-gray-100 hover:bg-black hover:text-whit focus:outline-none"
+        aria-expanded="false"
+        onClick={handlerTopic}
+      >
+        Añadir
+      </button>
+      {/* LISTADO */}
+      <ol className="mt-10">
         {temas.map((tema) => {
-          return <li>{tema}</li>;
+          return (
+            <li className="font-extra-bold font-medium text-xl">{tema}</li>
+          );
         })}
       </ol>
     </div>
--- proyectos/gifs-app/src/components/AddTopic.jsx
+++ proyectos/gifs-app/src/components/AddTopic.jsx
@@ -1,14 +1,34 @@
-export const AddTopic = () => {
-  const handlerInputTopic = (e) => {
-    console.log(e.target.value);
+import { useState } from "react";
+
+export const AddTopic = ({ setTemas, añadidos }) => {
+  // estado del input & handler
+  const [inputValue, setInputValue] = useState("");
+
+  const handlerInputSearch = (e) => {
+    // Seteamos el valor de inputValue
+    setInputValue(e.target.value);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // setInputValue([...añadidos, inputValue]);
+    if (inputValue.trim().length < 2) return;
+
+    setTemas([...añadidos, inputValue]);
+    // dejo la función limpia para cuando se vuelva a ejecutar
+    setInputValue("");
+  };
+
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Busca tus gifs por temas"
-        onChange={handlerInputTopic}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 mt-5 rounded-lg text-sm focus:outline-none"
+          type="text"
+          placeholder="Busca tus gifs por temas"
+          onChange={handlerInputSearch}
+        />
+      </form>
     </div>
   );
 };

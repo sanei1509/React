fee2b9c337772f1593a3018f7961b281849795fd
santiago <santiagoneira52@gmail.com>
2022-10-05 00:37:53 -0300

    avances del curso

--- proyectos/gifs-app/src/components/ListadoGifs.jsx
+++ proyectos/gifs-app/src/components/ListadoGifs.jsx
@@ -1,18 +1,28 @@
 // Si FUNCIONA ACA LA PODEMOS TENER EN CUALQUIER LUGAR APARTE
+import { useEffect } from "react";
 import { gifExtract } from "../helpers/ObtenerGifs";
 import { FirstCustom } from "../hooks/FirstCustom";
 
 const ListadoGifs = ({ key, tema }) => {
   // custom hook
-  const [images, cargandoContenido] = FirstCustom(tema);
+  const [images, setImages] = FirstCustom([]);
 
-  // const gifs = [1, 2, 3, 4, 5];
-  // Vamos a traer los gifs de la API
+  const getImages = async () => {};
+
+  useEffect(() => {
+    gifExtract(tema).then((images) => {
+      setImages(images);
+    });
+  }, []);
 
   return (
     <>
       <h3 className="text-3xl font-bold">{tema}</h3>
-      <img src={gifExtract(tema)["img"]} />
+      {/* <img src={gifExtract(tema)["img"]} /> */}
+      <li>titulo</li>
+      <li>titulo</li>
+      <li>titulo</li>
+      <li>titulo</li>
     </>
   );
 };

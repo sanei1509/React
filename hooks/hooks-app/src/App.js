b129f929217216ad9c1f913e4c46bc5d3cfb481c
santiago <santiagoneira52@gmail.com>
2022-10-05 12:11:56 -0300

    useState in depth, view possibly errors

---
+++ hooks/hooks-app/src/App.js
@@ -0,0 +1,12 @@
+import { Contador } from "./components/Contador.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Hooks</h1>
+      <Contador />
+    </div>
+  );
+}
+
+export default App;
---
+++ hooks/hooks-app/src/components/Contador.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export const Contador = () => {
+  const [contadores, setContador] = useState({
+    counter: 0,
+    counter2: 10,
+    counter3: 20,
+  });
+
+  const { counter, counter2, counter3 } = contadores;
+
+  return (
+    <>
+      <h2>{counter}</h2>
+      <button
+        onClick={() =>
+          setContador({
+            ...contadores,
+            counter: counter + 1,
+          })
+        }
+      >
+        +
+      </button>
+      <h2>{counter2}</h2>
+      <button
+        onClick={() =>
+          setContador({
+            ...contadores,
+            counter2: counter2 + 10,
+          })
+        }
+      >
+        +
+      </button>
+      <h2>{counter3}</h2>
+      <button
+        onClick={() => setContador({ ...contadores, counter3: counter3 + 20 })}
+      >
+        +
+      </button>
+    </>
+  );
+};

78df7db6c9726fe60d6a8adf469420adfdd114cc
santiago <santiagoneira52@gmail.com>
2022-10-10 11:32:42 -0300

    working in design of components to the final project

--- componentes-aislados/cards/src/App.jsx
+++ componentes-aislados/cards/src/App.jsx
@@ -1,18 +1,23 @@
-import NavBar from "./components/nav/NavBar";
+import NavBar from "./components/Nav/NavBar";
 // Componentes que vas a usar
-import "../src/components/nav/NavBar.css";
+import "./components/Nav/NavBar.css";
+import { ListadoLoops } from "./components/ListadoLoops/ListadoLoops";
 import image from "./assets/logo.png";
 import "./index.css";
 
 function App() {
   return (
     <>
+      {/* Logo */}
       <div className="Logo-container p-10">
         <a href="#Home" className="link-logo">
           <img src={image} alt="logo" width={160} />
         </a>
       </div>
+      {/* Barra izquierda */}
       <NavBar />
+      {/* Listado */}
+      <ListadoLoops />
     </>
   );
 }
---
+++ componentes-aislados/cards/src/components/ListadoLoops/ListadoLoops.jsx
@@ -0,0 +1,9 @@
+import "./ListadoLoops.css";
+
+export const ListadoLoops = () => {
+  return (
+    <div className="Container-loops">
+      <h1>Listado de loops</h1>
+    </div>
+  );
+};

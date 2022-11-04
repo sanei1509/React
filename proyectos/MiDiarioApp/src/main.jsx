e48adee7d302b1745909120dc37c1f32a2afbab6
santiago <santiagoneira52@gmail.com>
2022-11-04 08:10:11 -0300

    course avances

---
+++ proyectos/MiDiarioApp/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MiDiarioApp from "./MiDiarioApp";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MiDiarioApp />
+  </React.StrictMode>
+);

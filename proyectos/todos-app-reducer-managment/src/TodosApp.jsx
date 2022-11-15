277d58de64417b32da853ed5fffc0447a26f2825
santiago <santiagoneira52@gmail.com>
2022-11-15 20:22:11 -0300

    creating app with reducers manage

---
+++ proyectos/todos-app-reducer-managment/src/TodosApp.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export const TodosApp = () => {
+  return (
+    <div id="App">
+      <h1>Todos app</h1>
+    </div>
+  );
+};
---
+++ proyectos/todos-app-reducer-managment/src/reducers/firstReducers.js
@@ -0,0 +1,19 @@
+const initialState = [
+  {
+    id: 1,
+    name: "facu",
+  },
+  {
+    id: 2,
+    name: "Pipe",
+  },
+  {
+    id: 3,
+    name: "Cuqui",
+  },
+];
+
+// Mediante el reducer vamos a modificar el estado
+const firstReducers = () => {
+  return state;
+};
